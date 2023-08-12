5dd783a3352165ed17e8ddf398017f5c1bd21d6f
Tux <tux@birdeck.com>
2023-08-12 22:06:07 +0200

    refactor module loading

--- fah/Icon.py
+++ fah/Icon.py
@@ -19,6 +19,10 @@
 #                                                                              #
 ################################################################################
 
+import gi
+
+gi.require_version("Gtk", "3.0")
+
 from gi.repository import Gtk
 
 
